a936fa3ffc49dc5b1f7c166f917d379ba3fad9a9
Greg Luthman <gregory.allen.luthman@outlook.com>
2019-01-17 19:20:25 +0000

    adding DOM listeners

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-let Phrase = require("gluthman-palindrome");
+let Phrase = require("<username>-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing.");
-let phrase = new Phrase(string);
-if (phrase.palindrome()) {
-  alert(`${phrase.content} is a palindrome!`);
-} else {
-  alert(`${phrase.content} is not a palindrome.`);
-};
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
 
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
